0086d2759a9d520fbe2e8f480465add7a648a075
inth3wild <toochukeuchukwu@gmail.com>
2024-08-27 03:21:48 +0100

    feat: create reset.controller.ts skeleton file.

---
+++ backend/src/controllers/reset.controller.ts
@@ -0,0 +1,13 @@
+// import { Request, Response } from "express";
+// import jwt from "jsonwebtoken";
+// import User from '../database/models/my-user.model';
+// import ResetToken from '../database/models/reset-token.model';
+// import {generateResetTokenSchema, changePasswordSchema} from '../validators/reset.validator';
+
+// export const generateResetToken = async (req: Request, res: Response) => {
+
+// };
+
+// export const changePassword = async (req: Request, res: Response) => {
+
+// };
