37b68d44192d61e70bd67e807e6f12828712c54c
demola <ademolaabds@gmail.com>
2024-08-27 04:55:08 +0100

    created a reset page

---
+++ frontend/src/ResetPassword/Reset.tsx
@@ -0,0 +1,17 @@
+export default function Reset() {
+  return (
+    <div>
+      <div>
+        <h1>traidr</h1>
+        <p>Reset password</p>
+        <p>
+          Enter your Email below and we'll send you instructionson how to reset
+          your password
+        </p>
+        <label htmlFor="email"></label>
+        <input type="text" placeholder="johnDoe@gmail.com" />
+        <button>Send Rest Instructions</button>
+      </div>
+    </div>
+  );
+}
