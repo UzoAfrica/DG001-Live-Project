a4fd20a38f32a4b51b64d5a7e4e637f3a68de741
demola <ademolaabds@gmail.com>
2024-08-28 12:01:45 +0100

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Reset from './components/ResetPassword/Reset';
 import ReSend from './components/ResendOtp/Resend';
+import LogIn from './components/Login/LogInPage';
+import SignUp from './components/SignUp/SignUpPage';
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
+            <Route path="login" element={<LogIn />} />
+            <Route path="signup" element={<SignUp />} />
             <Route path="reset" element={<Reset />} />
             <Route path="resend" element={<ReSend />} />
+
             {/* <Route path="*" element={<NoPage />} /> */}
           </Route>
         </Routes>
@@ -18,24 +23,5 @@ function App() {
     </>
   );
 }
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import React from 'react';
-import LogIn from './components/Login/LogInPage';
-import SignUp from './components/SignUp/SignUpPage';
-
-const router = createBrowserRouter([
-  {
-    path: '/login',
-    element: <LogIn />,
-  },
-  {
-    path: '/signup',
-    element: <SignUp />,
-  },
-]);
-
-const App: React.FC = () => {
-  return <RouterProvider router={router} />;
-};
 
 export default App;
