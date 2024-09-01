4f608eafb306b0a4d9e2e456ca1eed1d08abed3d
inth3wild <toochukeuchukwu@gmail.com>
2024-09-01 02:15:36 +0100

    feat: add Product, Review and UserProducts models

---
+++ backend/src/database/models/review.model.ts
@@ -0,0 +1,37 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../config/sequelize.config';
+
+// Review model
+const Review = sequelize.define(
+  'Review',
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    indexes: [
+      // Create index on userName field
+      {
+        unique: true,
+        fields: ['userName'],
+      },
+    ],
+  }
+);
+
+export default Review;
