dc3dc83296de611d8b86aa3bb722e2f8b11449c8
demola <ademolaabds@gmail.com>
2024-09-10 12:42:54 +0100

    d

---
+++ frontend/src/components/AdminProfile/Adminprofile.tsx
@@ -0,0 +1,11 @@
+
+import AdminSettings from './AdminSettings';
+
+export default function AdminProfile() {
+  return (
+    <>
+      {/* <NavProfile /> */}
+      <AdminSettings />
+    </>
+  );
+}
