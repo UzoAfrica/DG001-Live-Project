8984f2cd1db2ac5f42d3aa649447947acaca9f55
demos-eleojo <eleojogabriel11@gmail.com>
2024-09-16 23:37:29 +0100

    product page bug fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,7 +43,7 @@ import ProductList from './components/ProductList';
 //tenantDb
 import TenantDb from "./admin/TenantDb.tsx";
 //productList
-import ProductList from '../src/components/ProductList.tsx'
+//import ProductList from '../src/components/ProductList.tsx'
 
 // Define an interface for MainLayout props
 interface MainLayoutProps {
